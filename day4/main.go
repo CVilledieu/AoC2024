32576f6c062751fcf5f7ba7ae973fee33cce01e5
Christopher V <ChrisRVill@gmail.com>
2024-12-28 00:23:46 -0800

    Finished day4 part 1 started day4 part 2

--- day4/main.go
+++ day4/main.go
@@ -14,22 +14,78 @@ Word can be horizontal, vertical, diagonal, backwards, or overlapping otherwords
 
 Goal answer: number of "XMAS"s
 
-(1,0)||(0,0)
-============
-(1,1)||(0,1)
+(-1,1)  (0,1)  (1,1)
+(-1,0) 	(x,y)  (1,0)
+(-1,-1) (0,-1) (1,-1)
 */
 
 const (
-	xmas = "XMAS"
+	xmas  = "MAS"
+	UP    = 1
+	DOWN  = -1
+	LEFT  = -1
+	RIGHT = 1
+	NONE  = 0
 )
 
-func main() {
-	grid := getGrid()
-	search(grid)
+type Graph struct {
+	Grid  [][]rune
+	Count int
 }
 
+func main() {
+	//Sets graph.grid from input.txt and sets graph.X/graph.Y to 0
+	graph := createGraph()
+	graph.seek()
+	fmt.Println(graph.Count)
+}
+
+func (g *Graph) seek() {
+	for y, r := range g.Grid {
+		for x, n := range r {
+			if n != 'A' {
+				continue
+			}
+			g.search(x, y)
+
+		}
+
+	}
+}
+
+func (G *Graph) NE_to_SW(x,y int) bool{
+	mas := []rune(xmas)
+	if G.Grid
+}
+func (G *Graph) NW_to_SE(x,y int){
+
+}
+
+func (G *Graph) search(x, y int) {
+	dX := []int{1, 1, -1, -1}
+	dY := []int{1, -1, -1, 1}
+	mas := []rune(xmas)
+	for i := range dX {
+		for j, n := range mas {
+			newX := x + (dX[i] * j)
+			newY := y + (dY[i] * j)
+			if newY < 0 || newX < 0 || newY >= len(G.Grid) || newX >= len(G.Grid[y]) {
+				break
+			}
+			if n != G.Grid[newY][newX] {
+				break
+			}
+			if j == 3 {
+				G.Count++
+			}
+		}
+	}
+}
+
+/*
 func search(grid [][]rune) {
 	var dX int
+	var dY int
 	length := len(xmas)
 	gridlength := len(grid)
 	//search diagonal
@@ -44,14 +100,19 @@ func search(grid [][]rune) {
 			if y-length < 0 || y+length > gridlength {
 				continue
 			}
-			dX++
+
 		}
 	}
 	fmt.Println(dX)
 }
+*/
+
+func createGraph() *Graph {
+	return &Graph{Grid: loadGrid(), Count: 0}
+}
 
 // theGrid is 140 by 140
-func getGrid() [][]rune {
+func loadGrid() [][]rune {
 	scanner := getScanner()
 	var line []rune
 	var grid [][]rune
